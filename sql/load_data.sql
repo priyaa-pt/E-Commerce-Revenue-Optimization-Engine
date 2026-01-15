d8c79a14e350d678af66400f57bed2ff1bd73767
Priya Tiwari <pt20022004@gmail.com>
2026-01-15 23:57:29 +0530

    load sql data 15/01

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -1,9 +1,12 @@
 USE ecommerce_analytics;
+SET GLOBAL local_infile = 1;
 
-LOAD DATA INFILE
+LOAD DATA LOCAL INFILE
 'E:/Projects/E-Commerce Revenue Optimization Engine/notebooks/cleaned_data.csv'
 INTO TABLE orders_clean
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+
